b714c7ad500721220a8680a90725047fee7de287
s8189882 <s8189882@gmail.com>
2022-08-24 14:39:55 +0530

    Add main branch - display welcome message

---
+++ Gambling Simulator/src/gambling/GamblingSimulator.java
@@ -0,0 +1,8 @@
+package gambling;
+
+public class GamblingSimulator {
+	public static void main(String[] args) {
+		System.out.println("Welcome to the Gambling Simulator in Java Program!");
+		System.out.println("This program simulates a Gambler who starts with a stake, and bets every game to win or to lose some stake. \nBeing a Calculative Gambler, he exits if the Stake reaches a high or a low limit.\n");
+	}
+}
