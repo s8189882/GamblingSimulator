968da48cfaffefb10cfd3099a7004ea72b71311e
s8189882 <s8189882@gmail.com>
2022-08-24 16:34:56 +0530

    Add UC1 - start with a stake of $100 every day and bet $1 every game

--- Gambling Simulator/src/gambling/GamblingSimulator.java
+++ Gambling Simulator/src/gambling/GamblingSimulator.java
@@ -1,8 +1,20 @@
 package gambling;
 
 public class GamblingSimulator {
+	public int funds = 0;
+	public int bet = 0;
+	
 	public static void main(String[] args) {
 		System.out.println("Welcome to the Gambling Simulator in Java Program!");
 		System.out.println("This program simulates a Gambler who starts with a stake, and bets every game to win or to lose some stake. \nBeing a Calculative Gambler, he exits if the Stake reaches a high or a low limit.\n");
+		GamblingSimulator gambler = new GamblingSimulator();
+		gambler.gameInitialise();
+	}
+	
+	public void gameInitialise() {
+		System.out.println("\nInitialising game...");
+		funds = 100;
+		bet = 1;
+		System.out.println("Funds : $"+funds);
 	}
 }
