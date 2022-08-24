fe2c144c7a453e8bfb2e1091e5f56de0ff7dd33a
s8189882 <s8189882@gmail.com>
2022-08-24 18:42:49 +0530

    Add UC7 - If won option to continue playing next month or stop Gambling

--- Gambling Simulator/src/gambling/GamblingSimulator.java
+++ Gambling Simulator/src/gambling/GamblingSimulator.java
@@ -1,5 +1,7 @@
 package gambling;
 
+import java.util.Scanner;
+
 public class GamblingSimulator {
 	public int funds = 0;
 	public int bet = 0;
@@ -7,25 +9,43 @@ public class GamblingSimulator {
 	public int cumulativeFunds = 0;
 	public int daysWon = 0;
 	public int daysLost = 0;
-	public boolean[] daysArray = new boolean [20];
-
+	public boolean[] daysArray = new boolean [21];
+	public boolean wonOrNot = false;
 	
 	public static void main(String[] args) {
 		System.out.println("Welcome to the Gambling Simulator in Java Program!");
 		System.out.println("This program simulates a Gambler who starts with a stake, and bets every game to win or to lose some stake. \nBeing a Calculative Gambler, he exits if the Stake reaches a high or a low limit.\n");
 		GamblingSimulator gambler = new GamblingSimulator();
 		gambler.gameInitialise();
-		gambler.dailyGamble();
-		gambler.displayTwentyDaysResult();
-		gambler.displayLuckyDays();
+		gambler.playGame();
+		Scanner sc = new Scanner(System.in);
+		if (gambler.wonOrNot) {
+			System.out.println("\n\nYOU HAVE WON!");
+			System.out.println("Congratulations!!!");
+			System.out.println("\nEnter 'Yes' to play for another month.");
+			System.out.println("\nEnter 'Stop' to quit the game.");
+			char gamingOption = sc.next().charAt(0);
+			if (gamingOption == 'y' || gamingOption == 'Y') {
+				gambler.gameInitialise();
+				gambler.playGame();
+			}
+			else if(gamingOption == 's' || gamingOption == 'S')
+				System.out.println("\nThank you for playing!");
+		}
+		else {
+			System.out.println("\n\nYOU HAVE LOST!");
+			System.out.println("You cannot play again.\nBetter luck next time.");
+		}
+		sc.close();
 	}
 	
 	public void gameInitialise() {
 		System.out.println("\nInitialising game...");
 		funds = 100;
 		bet = 1;
+		daysArray = null;
 		System.out.println("Funds : $"+funds);
-		}
+	}
 	
 	public void makeBet() {
 		double result = Math.random();
@@ -35,9 +55,8 @@ public class GamblingSimulator {
 			wins++;
 			betResult = true;
 		}
-		else {
+		else 
 			funds -= bet;
-		}
 		displayResults(betResult);
 	}
 	
@@ -79,8 +98,10 @@ public class GamblingSimulator {
 		System.out.println("\n\nFunds Stats after 20 days of Gambling...");
 		System.out.println("Number of days won funds :	"+daysWon);
 		System.out.println("Number of days lost funds :	"+daysLost);	
-		if (cumulativeFunds > (100*20))
+		if (cumulativeFunds > (100*20)) {
 			System.out.println("Total Funds Won :	$"+(cumulativeFunds-(100*20)));
+			wonOrNot = true;
+		}
 		else
 			System.out.println("Total Funds Lost :	$"+((100*20)-cumulativeFunds));
 		System.out.println("Total Funds Remaining :	$"+cumulativeFunds);
@@ -96,5 +117,11 @@ public class GamblingSimulator {
 		for (int i = 0; i < daysArray.length; i++)
 			if (!daysArray[i])
 				System.out.print("   Day "+(i+1));
+	}
+	
+	public void playGame() {
+		dailyGamble();
+		displayTwentyDaysResult();
+		displayLuckyDays();
 	}
 }
