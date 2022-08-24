5eb108cd3cb47a147c2d39b9e4e5b2d3b9f28371
s8189882 <s8189882@gmail.com>
2022-08-24 16:53:40 +0530

    Add UC2 - Gambler makes $1 bet and either wins or loses $1

--- Gambling Simulator/src/gambling/GamblingSimulator.java
+++ Gambling Simulator/src/gambling/GamblingSimulator.java
@@ -1,8 +1,45 @@
 package gambling;
 
 public class GamblingSimulator {
+	public int funds = 0;
+	public int bet = 0;
+	public int wins = 0;
+	
 	public static void main(String[] args) {
 		System.out.println("Welcome to the Gambling Simulator in Java Program!");
 		System.out.println("This program simulates a Gambler who starts with a stake, and bets every game to win or to lose some stake. \nBeing a Calculative Gambler, he exits if the Stake reaches a high or a low limit.\n");
+		GamblingSimulator gambler = new GamblingSimulator();
+		gambler.gameInitialise();
+		gambler.makeBet();
+	}
+	
+	public void gameInitialise() {
+		System.out.println("\nInitialising game...");
+		funds = 100;
+		bet = 1;
+		System.out.println("Funds : $"+funds);
+		}
+	
+	public void makeBet() {
+		double result = Math.random();
+		boolean betResult = false;
+		if (result > 0.5) {
+			funds += bet;
+			wins++;
+			betResult = true;
+		}
+		else {
+			funds -= bet;
+		}
+		displayResults(betResult);
+	}
+	
+	public void displayResults(boolean result) {
+		System.out.println();
+		if(result)
+			System.out.println("Bet WON!");
+		else
+			System.out.println("Bet LOST!");
+		System.out.println("Funds : $"+funds);
 	}
 }
