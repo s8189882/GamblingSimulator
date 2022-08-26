0f5ea7496d9bb0b1030fc818e60efbcd296edf7f
s8189882 <s8189882@gmail.com>
2022-08-26 17:08:30 +0530

    Add UC7 - continue game or stop

--- Gambling Simulator/src/gambling/GamblingSimulator.java
+++ Gambling Simulator/src/gambling/GamblingSimulator.java
@@ -9,41 +9,21 @@ public class GamblingSimulator {
 	public int cumulativeFunds = 0;
 	public int daysWon = 0;
 	public int daysLost = 0;
-	public boolean[] daysArray = new boolean [20];
+	public boolean[] daysArray = null;
 	public boolean wonOrNot = false;
 	
 	public static void main(String[] args) {
 		System.out.println("Welcome to the Gambling Simulator in Java Program!");
 		System.out.println("This program simulates a Gambler who starts with a stake, and bets every game to win or to lose some stake. \nBeing a Calculative Gambler, he exits if the Stake reaches a high or a low limit.\n");
 		GamblingSimulator gambler = new GamblingSimulator();
-		gambler.gameInitialise();
 		gambler.playGame();
-		Scanner sc = new Scanner(System.in);
-		if (gambler.wonOrNot) {
-			System.out.println("\n\nYOU HAVE WON!");
-			System.out.println("Congratulations!!!");
-			System.out.println("\nEnter 'Yes' to play for another month.");
-			System.out.println("\nEnter 'Stop' to quit the game.");
-			char gamingOption = sc.next().charAt(0);
-			if (gamingOption == 'y' || gamingOption == 'Y') {
-				gambler.gameInitialise();
-				gambler.playGame();
-			}
-			else if(gamingOption == 's' || gamingOption == 'S')
-				System.out.println("\nThank you for playing!");
-		}
-		else {
-			System.out.println("\n\nYOU HAVE LOST!");
-			System.out.println("You cannot play again.\nBetter luck next time.");
-		}
-		sc.close();
 	}
 	
 	public void gameInitialise() {
 		System.out.println("\nInitialising game...");
 		funds = 100;
 		bet = 1;
-		daysArray = null;
+		daysArray = new boolean [20];
 		System.out.println("Funds : $"+funds);
 	}
 	
@@ -105,7 +85,6 @@ public class GamblingSimulator {
 		else
 			System.out.println("Total Funds Lost :	$"+((100*20)-cumulativeFunds));
 		System.out.println("Total Funds Remaining :	$"+cumulativeFunds);
-			
 	}
 	
 	public void displayLuckyDays() {
@@ -120,8 +99,30 @@ public class GamblingSimulator {
 	}
 	
 	public void playGame() {
+		gameInitialise();
 		dailyGamble();
 		displayTwentyDaysResult();
 		displayLuckyDays();
+		continueGameOrStop();
+	}
+	
+	public void continueGameOrStop() {
+		Scanner sc = new Scanner(System.in);
+		if (wonOrNot) {
+			System.out.println("\n\nYOU HAVE WON!");
+			System.out.println("Congratulations!!!");
+			System.out.println("\nEnter 'Yes' to play for another month.");
+			System.out.println("\nEnter 'Stop' to quit the game.");
+			char gamingOption = sc.next().charAt(0);
+			if (gamingOption == 'y' || gamingOption == 'Y') 
+				playGame();
+			else if(gamingOption == 's' || gamingOption == 'S')
+				System.out.println("\nThank you for playing!");
+		}
+		else {
+			System.out.println("\n\nYOU HAVE LOST!");
+			System.out.println("You cannot play again.\nBetter luck next time.");
+		}
+		sc.close();
 	}
 }
